4a8f0b9f1a957e112c6a733a85f054eb7491ef64
ifazo <ziaulkarimifaz@outlook.com>
2024-03-05 10:59:06 +0600

    first commit

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Customer::factory()
+        ->count(10)
+        ->hasInvoices(10)
+        ->create();
+
+        Customer::factory()
+        ->count(10)
+        ->hasInvoices(5)
+        ->create();
+
+        Customer::factory()
+        ->count(10)
+        ->hasInvoices(1)
+        ->create();
+
+        Customer::factory()
+        ->count(10)
+        ->create();
+    }
+}
