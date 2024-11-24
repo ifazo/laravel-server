c3c76da19d74cc307ec6822eac2f8698aa87ff86
ifazo <ziaulkarimifaz@gmail.com>
2024-11-24 12:22:20 +0600

    category model done

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::factory()
+        ->count(10)
+        ->hasProducts(10)
+        ->create();
+
+        Category::factory()
+        ->count(10)
+        ->hasProducts(5)
+        ->create();
+
+        Category::factory()
+        ->count(10)
+        ->hasProducts(1)
+        ->create();
+
+        Category::factory()
+            ->count(10)
+            ->create();
+    }
+}
