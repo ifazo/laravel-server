eb41f5c7cbbcecd60680d06a8d1cb9e3ba99ef38
ifazo <ziaulkarimifaz@gmail.com>
2024-11-24 09:27:55 +0600

    full frontend and npm pkg remove

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h1>Welcome to the Laravel Server!</h1>";
+});
+
+Route::get('/api', function () {
+    echo "<h1>Server api is running successfully!</h1>";
 });
